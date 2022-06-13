816855d030f9cb06ddb5ca6c55e796409abeeb74
alyson albasi <aly@mcjerz.net>
2022-06-13 16:40:42 -0400

    fixes broken link

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 const okta = require("@okta/okta-sdk-nodejs");
 
 const client = new okta.Client({
-    orgUrl: "dev-29442551.okta.com",
+    orgUrl: "https://dev-29442551.okta.com",
     token: "00BEO8hq0h7a0T4bhCT18u9qPO_pDI56Hy88oYOP6D"
 });
 
